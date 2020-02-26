babdf32404f7735afddf2c293b176cdf30e70559
Mike Prior <mprior19xx@gmail.com>
2020-02-26 17:04:39 -0500

    setting up about page

--- public/js/modules/AboutComponent.js
+++ public/js/modules/AboutComponent.js
@@ -1,4 +1,55 @@
 export default {
     template: `
-    <h1>About Our Campaign</h1>
+    <div id="aboutCon">
+        <h1 class="hidden">About Our Campaign</h1>
+
+        <div class="aboutDescCon">
+            <h2 class="redTitle">What is #GetTested</h2>
+            <div class="aboutText">
+                <p>#GETTESTED (Get Tested) is a campaign encouraging young Canadians to get tested for HIV and AIDS. Our campaign aims to:</p>
+                <ul class="aboutList">
+                    <li>Make young Canadians aware of HIV and AIDS, and how to prevent them.</li>
+                    <li>Connect Canadians of all ages to testing services</li>
+                    <li>Promote an Open discussions with partners, peers and Health Care Providers</li>
+                </ul>
+            </div>
+
+            <div class="abtImageCon">Images will go here</div>
+
+            <div class="aboutText">
+                <p>Getting Tested for HIV / AIDs is important for all Canadians, regardless of race, gender, ethnicity or sexual preference. Although many young Canadians are not sexually active, learning the importance of preventing these diseases can make young people more comfortable when talking about their overall sexual health.</p>
+            </div>
+        </div>
+
+        <div class="aboutDescCon">
+            <h2 class="redTitle">Our Main Goal</h2>
+
+            <div class="aboutText">
+                <p>Our campaigns main goals are to:</p>
+                <ul class="aboutList">
+                    <li>Make young Canadians more aware of the benefits of testing for sexually transmitted diseases.</li>
+                    <li>Provide proper links and information about HIV and AIDS testing to young people
+                    </li>
+                    <li>Make testing an act of pride, and a vital part of a young person's lifestyle, in order to reduce the fear and or shame they may feel about testing. </li>
+                    <li>Empower and Inspire young Canadians to know who they are, Including their HIV, AIDS and STD status. </li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="aboutDescCon"">
+            <h2 class="redTitle">Why is This Necessary?</h2>
+
+            <div class="aboutText">
+                <p>A campaign like ours can help young people feel pride and encouragement in taking control of their sexual health. It also aids in helping make them aware of the necessary prevention methods against HIV, AIDS and STD's. Having proper access to factual information can aid young Canadians on understanding how important it is to get tested if they are having or are thinking about having sexual interactions. </p>
+            </div>
+        </div>
+
+        <div class="abtImageCon">Images will go here</div>
+
+        <div class="contactCon">
+        <h2 class="whiteTitle">Get Involved</h2>
+        </div>
+
+
+    </div>
     `}
