ab51c12c78196fee700af020c50591cbd5a791be
Mike Prior <mprior19xx@gmail.com>
2020-02-26 20:02:26 -0500

    added homepage structure

--- public/js/modules/HomeComponent.js
+++ public/js/modules/HomeComponent.js
@@ -1,9 +1,69 @@
 export default {
     template: `
-
+<div id="homeCon">
     <h1 class="hidden">Home Page</h1>
+    <img id="homeLogo" src="./public/images/fip_logo.svg">
+
+    <div id="videoCon">
+        <h2 class="whiteTitle">Video Goes Here</h2>
+    </div>
+    
+    <div id="homeDesc">
+        <h2 class="redTitle">A Bit About Us</h2>
+        <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+    </div>
+
+    <div class="topicBox" id="box1">
+        <h2 class="whiteTitle">Topic One</h2>
+
+        <div class="downArrow">
+            <i class="fas fa-sort-down fa-4x"></i>
+        </div>
+    </div>
+
+    <div class="topicBox" id="box2">
+        <h2 class="whiteTitle">Topic Two</h2>
+
+        <div class="downArrow">
+            <i class="fas fa-sort-down fa-4x"></i>
+        </div>
+    </div>
+
+    <div class="topicBox" id="box3">
+        <h2 class="whiteTitle">Topic Three</h2>
+
+        <div class="downArrow">
+            <i class="fas fa-sort-down fa-4x"></i>
+        </div>
+    </div>
+
+    <div class="topicBox" id="box4">
+        <h2 class="whiteTitle">Topic Four</h2>
+
+        <div class="downArrow">
+            <i class="fas fa-sort-down fa-4x"></i>
+        </div>
+    </div>
+
+    <div class="topicBox" id="box5">
+        <h2 class="whiteTitle">Topic Five</h2>
+
+        <div class="downArrow">
+            <i class="fas fa-sort-down fa-4x"></i>
+        </div>
+    </div>
+
+    <div class="topicBox" id="box6">
+        <h2 class="whiteTitle">Topic Six</h2>
+
+        <div class="downArrow">
+            <i class="fas fa-sort-down fa-4x"></i>
+        </div>
+    </div>
 
 
 
+
+</div>
     `
 }
