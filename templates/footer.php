60a36e795a8b5ce2a49a5d5ded02dea853180359
Mike Prior <mprior19xx@gmail.com>
2020-03-10 20:11:07 -0400

    restyled the home page for the most part.

--- templates/footer.php
+++ templates/footer.php
@@ -3,11 +3,17 @@
     <img id="footerLogo" src="public/images/fip_logo.svg" alt="logo">
 
     <div id="footerNav">
+
         <router-link class="footerLink" to="/">Home</router-link>
-        <router-link class="footerLink" to="/about">About</router-link>
+
+        <router-link class="footerLink" to="/about">Get The Facts</router-link>
+
         <router-link class="footerLink" to="/contact">Get Involved</router-link>
+
         <!-- <router-link class="footerLink" to="/login">Admin</router-link> -->
+
         <a class="footerLink" href="admin/admin_login.php">Admin Login</a>
+        
     </div>
     
     <div id="copyright"> Copyright <?php echo date('Y'); ?> </div>
