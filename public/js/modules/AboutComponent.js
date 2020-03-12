130e784e931a2bd624851afbf024305db12dcee3
Mike Prior <mprior19xx@gmail.com>
2020-03-11 20:59:17 -0400

    most styling done on about page

--- public/js/modules/AboutComponent.js
+++ public/js/modules/AboutComponent.js
@@ -1,88 +1,68 @@
 export default {
     template: `
-    <div id="aboutCon">
-        <h1 class="hidden">About Our Campaign</h1>
-
-        <div class="aboutDescCon">
-            <h2 class="redTitle">What is #GetTested</h2>
-            <div class="aboutText">
-                <p>#GETTESTED (Get Tested) is a campaign encouraging young Canadians to get tested for HIV and AIDS. Our campaign aims to:</p>
-                <ul class="aboutList">
-                    <li>Make young Canadians aware of HIV and AIDS, and how to prevent them.</li>
-                    <li>Connect Canadians of all ages to testing services</li>
-                    <li>Promote an Open discussions with partners, peers and Health Care Providers</li>
-                </ul>
-            </div>
-
-            <div class="abtImageCon">Images will go here</div>
-
-            <div class="aboutText">
-                <p>Getting Tested for HIV / AIDs is important for all Canadians, regardless of race, gender, ethnicity or sexual preference. Although many young Canadians are not sexually active, learning the importance of preventing these diseases can make young people more comfortable when talking about their overall sexual health.</p>
-            </div>
-        </div>
-
-        <div class="aboutDescCon">
-            <h2 class="redTitle">Our Main Goal</h2>
-
-            <div class="aboutText">
-                <p>Our campaigns main goals are to:</p>
-                <ul class="aboutList">
-                    <li>Make young Canadians more aware of the benefits of testing for sexually transmitted diseases.</li>
-                    <li>Provide proper links and information about HIV and AIDS testing to young people
-                    </li>
-                    <li>Make testing an act of pride, and a vital part of a young person's lifestyle, in order to reduce the fear and or shame they may feel about testing. </li>
-                    <li>Empower and Inspire young Canadians to know who they are, Including their HIV, AIDS and STD status. </li>
-                </ul>
-            </div>
-        </div>
-
-        <div class="aboutDescCon"">
-            <h2 class="redTitle">Why is This Necessary?</h2>
-
-            <div class="aboutText">
-                <p>A campaign like ours can help young people feel pride and encouragement in taking control of their sexual health. It also aids in helping make them aware of the necessary prevention methods against HIV, AIDS and STD's. Having proper access to factual information can aid young Canadians on understanding how important it is to get tested if they are having or are thinking about having sexual interactions. </p>
-            </div>
-        </div>
-
-        <div class="abtImageCon">Images will go here</div>
-
-        <div class="contactCon">
-            <h2 class="whiteTitle">Get Involved</h2>
-
-            <div id="contactText">
-                <p>For more information about our campaign efforts, or how to get yourself involved, please contact us though the information provided below, or contact your local Health Clinic for more information.</p>
-
-                <ul>
-                    <li>By Phone : +1 555-555-5555
-                    </li>
-                    <li>By Email : contact@gettested.ca
-                    </li>
-                    <li>By Fax : +1 555-555-5555
-                    </li>
-                    <li>In Person : 123 Main Street, 
-                    London On, Canada</li>
-                </ul>
-            </div>
-
-            <h2 class="whiteTitle">Find Us On Social Media</h2>
-
-            <div id="socialCon">
-                <a class="socialIcon" href="http://www.facebook.com">
-                    <i class="fab fa-facebook fa-3x"></i>
-                </a>
-                <a class="socialIcon" href="http://www.twitter.com">
-                    <i class="fab fa-twitter-square fa-3x"></i>
-                </a>
-                <a class="socialIcon" href="http://www.youtube.com">
-                    <i class="fab fa-youtube fa-3x"></i>
-                </a>
-                <a class="socialIcon" href="http://www.google.com">
-                    <i class="fab fa-google fa-3x"></i>
-                </a>
-            </div>
-
-        </div>
-
-
+<div id="aboutCon">
+    <h1 class="hidden">About Our Campaign</h1>
+    
+    <div class="aboutIntro">
+        <p id="introText">#GETTESTED (Get Tested) is a campaign encouraging young Canadians to get tested for HIV and AIDS.</p>
     </div>
-    `}
+
+    <div class="aboutPopUp">
+        <div class="blueBorder">
+            <h3 class="pink"> Our Campaign Aims To:</h3>
+
+            <ul class="aboutList">
+                <li class="smBlueText">Make young Canadians aware of HIV and AIDS, and how to prevent them.</li><br>
+                <li class="smBlueText">Connect Canadians of all ages to testing services</li><br>
+                <li class="smBlueText">Promote an Open discussions with partners, peers and Health Care Providers</li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="aboutPopUp">
+        <div class="blueBorder">
+            <p class="smBlueText">Getting Tested for HIV / AIDs is important for all Canadians, regardless of race, gender, ethnicity or sexual preference. Although many young Canadians are not sexually active, learning the importance of preventing these diseases can make young people more comfortable when talking about their overall sexual health.</p>
+        </div>
+    </div>
+
+    <div class="aboutPopUp">
+        <div class="blueBorder">
+            <h3 class="pink">Our Goals</h3>
+
+            <ul class="aboutList">
+                <li class="smBlueText">Make young Canadians more aware of the benefits of testing for sexually transmitted diseases.</li><br>
+                <li class="smBlueText">Provide proper links and information about HIV and AIDS testing to young people
+                </li><br>
+                <li class="smBlueText">Make testing an act of pride, and a vital part of a young person's lifestyle, in order to reduce the fear and or shame they may feel about testing. </li><br>
+                <li class="smBlueText">Empower and Inspire young Canadians to know who they are, Including their HIV, AIDS and STD status. </li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="aboutPopUp">
+        <div class="blueBorder">
+            <p class="smBlueText">A campaign like ours can help young people feel pride and encouragement in taking control of their sexual health. It also aids in helping make them aware of the necessary prevention methods against HIV, AIDS and STD's. Having proper access to factual information can aid young Canadians on understanding how important it is to get tested if they are having or are thinking about having sexual interactions. </p>
+        </div>
+    </div>
+
+    <div class="aboutPopUp">
+        <div class="blueBorder">
+            <h3 class="pink">Get Involved</h3>
+
+            <ul class="aboutList">
+                <li class="smBlueText">By Phone : +1 555-555-5555</li><br>
+                <li class="smBlueText">By Email : contact@gettested.ca
+                    </li><br>
+                <li class="smBlueText">By Fax : +1 555-555-5555 </li><br>
+                <li class="smBlueText">In Person : 123 Main Street, 
+                    London On, Canada</li>
+            </ul>
+        </div>
+    </div>
+
+    <div id="socialCon">
+        social media icons here
+    </div>
+
+</div>
+`}
--- public/js/modules/HomeComponent.js
+++ public/js/modules/HomeComponent.js
@@ -6,54 +6,56 @@ export default {
     <div id="heroCon"> 
         <img src="./public/images/heroImgWeb.svg" alt="hero image with text" id="heroImage">
 
-        <i class="fas fa-chevron-down fa-3x" id="chevronDown"></i>
+        <i class="fas fa-chevron-down fa-3x" id="chevronDown"> </i>
     </div>
 
-    <div id="popUpCon">
+    <div id="homeBottom">
 
-        <div class="popUpLeft" id="popUp1">
+        <div class="popUp" id="popUp1">
             <div class="blueBorder">
-                <h3 class="pinkTitle">Get Yourself Tested!</h3>
+                <h3 class="pink"> Get Yourself Tested! </h3>
 
-                <p> Knowing Your HIV and STD status helps you choose options to stay healthy! </p>
+                <p class="smBlueText"> Knowing Your HIV and STD status helps you choose options to stay healthy! </p>
             </div>
         </div>
 
-        <div class="popUpRight" id="popUp2">
+        <div class="popUp" id="popUp2">
             <div class="blueBorder">
-                <h3 class="pinkTitle">Talk About PeEP!</h3>
+
+                <h3 class="pink">Talk About PeEP!</h3>
 
                 <p class="smBlueText">Talk to your doctor abuot pre-exposure prophylaxis (PrEP) as it can greatly reduce your risk of infection. </p>
             </div>
         </div>
 
-        <div class="popUpLeft" id="popUp3">
+        <div class="popUp" id="popUp3">
             <div class="blueBorder">
-                <h3 class="pinkTitle">Use A Condom!</h3>
 
-                <p class="smBlueText">Condoms are highly effective at preventing both HIV, AIDS and other STDs.</p>
+                <h3 class="pink">Use A Condom!</h3>
+
+                <p class="smBlueText"> Condoms are highly effective at preventing both HIV, AIDS and other STDs.</p>
             </div>
         </div>
 
-        <div class="popUpRight" id="popUp4">
+        <div class="popUp" id="popUp4">
             <div class="blueBorder">
-                <h3 class="pinkTitle">HIV Treatment!</h3>
+                <h3 class="pink">HIV Treatment!</h3>
 
                 <p class="smBlueText">HIV treatment keeps you healthy and minimizes the risk of spreading the disease to others.</p>
             </div>
         </div>
 
-        <div class="popUpLeft" id="popUp5">
+        <div class="popUp" id="popUp5">
             <div class="blueBorder">
-                <h3 class="pinkTitle">Don't Inject Drugs!</h3>
+                <h3 class="pink">Don't Inject Drugs!</h3>
 
                 <p class="smBlueText">But if you do,only use sterile equiptment and water. Most importantly, never share your gear!</p>
             </div>
         </div>
-        
-    </div>
 
-    <button class="button"> <span class="buttonTxt"> Get The Facts </span></button>
+        <button class="button"> <span class="buttonTxt"> Get The Facts </span></button>
+
+    </div>
 
 </div>
 
