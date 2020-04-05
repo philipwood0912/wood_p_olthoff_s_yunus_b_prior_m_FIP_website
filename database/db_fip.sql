73860e6147f4ba564d5f0b2a00065fcb266cd6f5
Philip <philipwood0912@gmail.com>
2020-04-05 03:02:24 -0400

    db update

--- database/db_fip.sql
+++ database/db_fip.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Mar 31, 2020 at 05:47 AM
+-- Generation Time: Apr 05, 2020 at 07:00 AM
 -- Server version: 5.7.26
 -- PHP Version: 7.3.8
 
@@ -19,6 +19,29 @@ SET time_zone = "+00:00";
 --
 -- Database: `db_fip`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_about`
+--
+
+CREATE TABLE `tbl_about` (
+  `ID` int(11) NOT NULL,
+  `Title` varchar(63) NOT NULL,
+  `Text` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `tbl_about`
+--
+
+INSERT INTO `tbl_about` (`ID`, `Title`, `Text`) VALUES
+(1, 'Our Campaign Aims To:', 'Make young Canadians aware of HIV and AIDS, and how to prevent them.^Connect Canadians of all ages to testing services.^Promote an Open discussions with partners, peers and Health Care Providers.'),
+(2, 'Title 2:', 'Getting Tested for HIV / AIDs is important for all Canadians, regardless of race, gender, ethnicity or sexual preference.^Although many young Canadians are not sexually active, learning the importance of preventing these diseases can make young people more comfortable when talking about their overall sexual health.'),
+(3, 'Our Goals:', 'Make young Canadians more aware of the benefits of testing for sexually transmitted diseases.^Provide proper links and information about HIV and AIDS testing to young people.^Make testing an act of pride, and a vital part of a young person\'s lifestyle, in order to reduce the fear and or shame they may feel about testing.^Empower and Inspire young Canadians to know who they are, Including their HIV, AIDS and STD status.'),
+(4, 'Title 4:', 'A campaign like ours can help young people feel pride and encouragement in taking control of their sexual health.^It also aids in helping make them aware of the necessary prevention methods against HIV, AIDS and STD\'s.^Having proper access to factual information can aid young Canadians on understanding how important it is to get tested if they are having or are thinking about having sexual interactions.'),
+(5, 'Get Involved:', 'By Phone : +1 555-555-5555^By Email : contact@gettested.ca^By Fax : +1 555-555-5555^In Person : 123 Main Street, London On, Canada');
 
 -- --------------------------------------------------------
 
@@ -66,9 +89,9 @@ INSERT INTO `tbl_clinics` (`ID`, `Clinic_Name`, `Address`, `Phone`, `Website`, `
 
 CREATE TABLE `tbl_home` (
   `ID` int(11) NOT NULL,
-  `Title` varchar(64) NOT NULL,
+  `Title` varchar(63) NOT NULL,
   `Text` text NOT NULL,
-  `Image` varchar(60) NOT NULL
+  `Image` varchar(63) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -110,6 +133,12 @@ INSERT INTO `tbl_users` (`ID`, `F_Name`, `L_Name`, `Email`, `User_Name`, `User_P
 --
 
 --
+-- Indexes for table `tbl_about`
+--
+ALTER TABLE `tbl_about`
+  ADD PRIMARY KEY (`ID`);
+
+--
 -- Indexes for table `tbl_clinics`
 --
 ALTER TABLE `tbl_clinics`
@@ -132,6 +161,12 @@ ALTER TABLE `tbl_users`
 --
 
 --
+-- AUTO_INCREMENT for table `tbl_about`
+--
+ALTER TABLE `tbl_about`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
 -- AUTO_INCREMENT for table `tbl_clinics`
 --
 ALTER TABLE `tbl_clinics`
@@ -141,7 +176,7 @@ ALTER TABLE `tbl_clinics`
 -- AUTO_INCREMENT for table `tbl_home`
 --
 ALTER TABLE `tbl_home`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `tbl_users`
